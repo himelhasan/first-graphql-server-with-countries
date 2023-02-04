4fdcc31388df7554ff92af51f22cd44155c731a7
himelhasan <himelhasan497@gmail.com>
2023-02-04 14:43:52 +0600

    movies api added

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,17 +1,28 @@
-const { UserList } = require("../FakeData");
+const { UserList, MovieList } = require("../FakeData");
 const _ = require("lodash");
 
 const resolvers = {
   Query: {
-    // USER RESOLVERS
-
+    // USER RESOLVERS for all the users
     users: () => {
       return UserList;
     },
+    // single user using argument
     user: (parent, args) => {
       const id = args.id;
       const user = _.find(UserList, { id: Number(id) });
       return user;
+    },
+    // Movie resolvers for all the movies
+    movies: () => {
+      return MovieList;
+    },
+
+    // single Movie resolvers for all the movies
+    movie: (parent, args) => {
+      const name = args.name;
+      const movie = _.find(MovieList, { name: name });
+      return movie;
     },
   },
 };
